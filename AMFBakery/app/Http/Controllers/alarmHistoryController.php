f0c4a66f18dbca307a2dd328ab8816d8f422c1b9
JasperRutte <jasperrutte@gmail.com>
2024-10-21 22:25:02 +0200

    made laravel file, read out data

---
+++ AMFBakery/app/Http/Controllers/alarmHistoryController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class alarmHistoryController extends Controller
+{
+    public function show()
+    {
+        // Path to (for now) csv file
+        // note that this is only temporary, later on we will try to import from the webpage
+        // instead of using the manual way
+        $filePath = storage_path('app/public/AlarmHistory.csv');
+
+        $handle = fopen($filePath, 'r');
+
+        if (!$handle) {
+            abort(404, 'File not found');
+        }
+
+        // read out the csv file
+        $data = [];
+        $chunkSize = 1000; // define amount of rows you want to be returned
+
+        while (($row = fgetcsv($handle)) !== false) {
+            // Add row to data (or process it)
+            $data[] = $row;
+
+            // Puts a limit to only loading the amount of chunk size
+            if (count($data) >= $chunkSize) {
+                break;
+            }
+        }
+
+        fclose($handle);
+
+        return view('csv.show', compact('data'));
+    }
+}
