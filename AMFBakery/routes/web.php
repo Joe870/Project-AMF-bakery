752f2f2ef24a856cc7cef9d358e43c594b596ea4
Noa <99072699@mydavinci.nl>
2024-11-26 12:49:54 +0100

    loginSystem

--- AMFBakery/routes/web.php
+++ AMFBakery/routes/web.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\alarmHistoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
@@ -16,4 +14,3 @@ Route::view('profile', 'profile')
 require __DIR__.'/auth.php';
 
 Route::get('/csv', [alarmHistoryController::class, 'show']);
-Route::get('/login', [alarmHistoryController::class, 'show']);//Change to go to login view
