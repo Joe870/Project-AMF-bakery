bff052a61b38e082d7bfa121b92dd90fe2e9d230
JasperRutte <jasperrutte@gmail.com>
2024-11-26 11:24:37 +0100

    old

--- AMFBakery/app/Livewire/DashboardChartBox.php
+++ AMFBakery/app/Livewire/DashboardChartBox.php
@@ -7,9 +7,9 @@ use Livewire\Component;
 class DashboardChartBox extends Component
 {
     public $errors = ['ERROR 123', 'ERROR 456', 'ERROR 789'];//test errors
-    
 
-    
+
+
     public function render()
     {
         return view('livewire.dashboard-chart-box');
